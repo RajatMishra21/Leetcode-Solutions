ba9b66d3d5daa251c8f8f3319a6ebe23e7fdd0fe
Rajat Mishra <94281732+RajatMishra21@users.noreply.github.com>
2022-09-18 14:50:34 +0530

    Time: 2 ms (39.50%), Space: 57.6 MB (30.73%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -3,11 +3,12 @@ class Solution {
         int left=0;
         int right=s.length-1;
         while(left<right){
-            char temp=s[right];
-            s[right--]=s[left];
-            s[left++]=temp;
+            char temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            right--;
+            left++;
         }
-            
         
-}
+    }
 }
